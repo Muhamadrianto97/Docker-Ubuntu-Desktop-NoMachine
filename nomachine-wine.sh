3fb49acc3fe81529ef1437b5d586bfd84ae5d0d3
Muhamad Rianto <muhamadrianto97@gmail.com>
2025-02-12 14:27:10 +0700

    Update nomachine-wine.sh

--- nomachine-wine.sh
+++ nomachine-wine.sh
@@ -13,11 +13,10 @@ function goto
     exit
 }
 
-: ngrok
+: loclx
 clear
-echo "Go to: https://dashboard.ngrok.com/get-started/your-authtoken"
-read -p "Paste Ngrok Authtoken: " CRP
-./ngrok config add-authtoken $CRP 
+echo "https://localxpose.io/dashboard/access"
+./loclx account login
 
 clear
 echo "Repo: https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine"
@@ -32,9 +31,9 @@ echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 4000 &>/dev/null &
+./loclx tunnel tcp --region $CRP 4000 &>/dev/null &
 sleep 1
-if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "loclx Error! Please try again!" && sleep 1 && goto ngrok; fi
 docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:wine
 clear
 echo "NoMachine: https://www.nomachine.com/download"
