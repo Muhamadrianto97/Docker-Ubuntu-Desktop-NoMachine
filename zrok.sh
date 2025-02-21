42ae38e2b1a78d58bc6154893ade23c11042452b
Muhamad Rianto <muhamadrianto97@gmail.com>
2025-02-21 09:37:35 +0700

    Update zrok.sh

--- zrok.sh
+++ zrok.sh
@@ -6,7 +6,7 @@ stty susp undef
 clear
 rm -rf zrok zrok.zip zrok.sh > /dev/null 2>&1
 echo "======================="
-echo "Downloading ngrok..."
+echo "Downloading zrok..."
 echo "======================="
 wget -O zrok.tgz https://github.com/openziti/zrok/releases/download/v0.4.48/zrok_0.4.48_linux_amd64.tar.gz > /dev/null 2>&1
 tar -xvzf zrok.tgz > /dev/null 2>&1
