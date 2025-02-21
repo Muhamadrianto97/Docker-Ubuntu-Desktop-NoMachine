4ca8b0ca82f067337be47c0e8a23870a5c3c5327
Muhamad Rianto <muhamadrianto97@gmail.com>
2025-02-21 09:36:09 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -6,7 +6,7 @@ stty susp undef
 clear
 rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
 echo "======================="
-echo "Downloading ngrok..."
+echo "Downloading zrok..."
 echo "======================="
 wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 tar -xvzf ngrok.tgz > /dev/null 2>&1
